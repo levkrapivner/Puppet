3bb660e2e78c1e08efaa35c55c66fba66334d6c9
levkrapivner <skyrim12345@yahoo.com>
2016-07-17 23:58:50 +0300

    Create client.py
    basic client

---
+++ client.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+from socket import *
+
+client = socket(AF_INET,SOCK_STREAM)            # select socket type
+client.connect(("ip",port))                     # connect to the server
+
+while True:
+    client.sendall(raw_input("root@debian: ~ # "))  # send the command to the server
+    print client.recv(4096)                         # receive the data from the server
+
+client.close()                                  # you need to close the socket, but!! you are the client so who cares...
