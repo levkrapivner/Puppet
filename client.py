896fee246f22c59d3ddc9a640df15546d36ae400
levkrapivner <skyrim12345@yahoo.com>
2016-07-17 23:59:40 +0300

    Update client.py

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ client = socket(AF_INET,SOCK_STREAM)            # select socket type
 client.connect(("ip",port))                     # connect to the server
 
 while True:
-    client.sendall(raw_input(""))  # send the command to the server
+    client.sendall(raw_input(""))                    # send the command to the server
     print client.recv(4096)                         # receive the data from the server
 
 client.close()                                  # you need to close the socket, but!! you are the client so who cares...
