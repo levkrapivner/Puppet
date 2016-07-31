0e30e99498a37b57263ae7b2813fac8c9a4d839a
levkrapivner <skyrim12345@yahoo.com>
2016-08-01 01:06:50 +0300

    update server.py

--- server.py
+++ server.py
@@ -1,24 +1,48 @@
 #!/usr/bin/python
-from datetime import *
+
 from functions import *
-#get the time
-current_time =  datetime.now().strftime('%H:%M:%S %Y-%m-%d')
 
-print """
+project_name()
+# create a socket object
+serversocket = socket.socket(
+	        socket.AF_INET, socket.SOCK_STREAM)
 
-################################################################################
+host = "lev-pc"
+port = 9999
 
-Python 102 name: Lev [%s]
+# bind to the port
+serversocket.bind((host, port))
 
-################################################################################
+# queue up to 5 requests
+serversocket.listen(5)
 
-""" % current_time
-print """
-Segment: %s
+clientsocket,addr = serversocket.accept()
+while True:
+    choice = menu()
+    if choice == "1":
+        clientlist = []
+        if addr not in clientlist:
+            clientlist.append(addr)
+            print clientlist
+        back2menu = raw_input("Back To Main Menu? >> y/n")
+        if back2menu == "y":
+            menu()
+        elif back2menu == type(int):
+            print "please choose y or n !!!"
+        else:
+            print "goodbuy!"
+            clientsocket.close()
+            break
+    elif choice == "2":
+        print 2
+    elif choice == "3":
+        print 3
+    elif choice == "4":
+        print 4
+    elif choice == "5":
+        print 5
+    else:
+        print "What about a bit of thinking?????"
 
-Your dns:
-
-Your gateway:
-
-##############################
-""" % get_ip()
+clientsocket.close()
+serversocket.close()
